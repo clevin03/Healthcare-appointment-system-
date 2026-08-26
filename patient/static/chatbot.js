8a789092b58b5a47063c83e8b6841d256a1e4c8d
Clevin <clevinfer@gmail.com>
2026-08-26 17:41:58 +0530

    Update booking page redirection from book_appointment.php to sessions.php

--- patient/static/chatbot.js
+++ patient/static/chatbot.js
@@ -292,7 +292,7 @@ function handleMentalHealthActions(actions) {
 }
 
 function openBookingPage() {
-    window.location.href = 'book_appointment.php';
+    window.location.href = 'sessions.php';
 }
 
 function handleActionClick(action) {
